e114df11ee13ebc52efd365837e7d57d32f5a7cf
xyh <eigo.shou@icloud.com>
2026-09-22 20:14:56 +0900

    替换系统User类

--- meiduo_mall/apps/users/models.py
+++ meiduo_mall/apps/users/models.py
@@ -5,10 +5,10 @@ from django.db import models
 # 1.自己定义模型
 # 密码我们要加密，还要实现登录的时候密码的验证
 
-class User(models.Model):
-    username = models.CharField(max_length=20,unique=True)
-    password = models.CharField(max_length=20)
-    mobile = models.CharField(max_length=11,unique=True)
+# class User(models.Model):
+#     username = models.CharField(max_length=20,unique=True)
+#     password = models.CharField(max_length=20)
+#     mobile = models.CharField(max_length=11,unique=True)
 
 # 2.django自带一个用户模型
 # 这个用户模型 有密码的加密 和密码的验证
@@ -21,3 +21,6 @@ class User(AbstractUser):
         db_table = 'tb_user'
         verbose_name = '用户管理'
         verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
--- meiduo_mall/meiduo_mall/settings.py
+++ meiduo_mall/meiduo_mall/settings.py
@@ -194,3 +194,6 @@ LOGGING = {
         },
     }
 }
+
+# 指定本项目用户模型类
+AUTH_USER_MODEL = 'users.User'
