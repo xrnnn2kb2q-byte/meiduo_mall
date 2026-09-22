fc04a7c59c688ff37d745a763d5303a9db2ca31b
xyh <eigo.shou@icloud.com>
2026-09-22 15:41:58 +0900

    log日志配置

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -15,8 +15,23 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
+
+def log(request):
+    # 1. 导入
+    import logging
+    # 2. 创建日志器
+    logger = logging.getLogger('django')
+    # 3. 调用日志器的方法来保存信息
+    logger.info('用户登录了')
+    logger.warning('redis缓存不足')
+    logger.error('该记录不存在')
+    logger.debug('~~~~~~~~~~~~~~~')
+    
+    return HttpResponse('log')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('log/',log)
 ]
