628641f20c339329d2dfa4b4345e06487d8fe9ff
xyh <eigo.shou@icloud.com>
2026-09-22 20:04:16 +0900

    定义用户模型类

--- meiduo_mall/apps/users/views.py
+++ meiduo_mall/apps/users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+    需求分析：根据页面的功能（从上到下，从左到右），哪些功能需要和后端配合完成
+    如何确定 哪些功能需要和后端进行交互呢？
+        1.经验
+        2.关注类似网址的相似功能
+        
+"""
